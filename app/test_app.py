beddd850acc6a8aaac51ed0cb3258acdeb76e239
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-03 16:14:00 +0300

    Add pytest

---
+++ app/test_app.py
@@ -0,0 +1,21 @@
+from app import app
+
+def test_index():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_results():
+    client = app.test_client()
+    response = client.get('/results')
+    assert response.status_code == 200
+
+def test_results():
+    client = app.test_client()
+    response = client.get('/remove')
+    assert response.status_code == 200
+
+def test_results():
+    client = app.test_client()
+    response = client.get('/update')
+    assert response.status_code == 200
