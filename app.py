6a907139320082d56bd38e3cd82c21e34e7069de
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-09 21:33:16 +0300

    Update ci-workflow

--- app.py
+++ app.py
@@ -13,16 +13,16 @@ column_names = ["id", "weather_state_name", "wind_direction_compass", "created",
                     "applicable_date", "min_temp", "max_temp", "the_temp"]
 
 db_params = {
-    # "host": os.getenv('DB_HOST'),
-    # "database": os.getenv('DB_NAME'),
-    # "user": os.getenv('DB_USER'),
-    # "password": os.getenv('DB_PASSWORD'),
-    # "port": "5432"
-    "host": "terraform-2021080914244468220000000a.cij2bgzi6jqj.us-east-1.rds.amazonaws.com",
-    "database": "weather",
-    "user": "epam",
-    "password": "SSpassword",
+    "host": os.getenv('DB_HOST'),
+    "database": os.getenv('DB_NAME'),
+    "user": os.getenv('DB_USER'),
+    "password": os.getenv('DB_PASSWORD'),
     "port": "5432"
+    # "host": "terraform-2021080914244468220000000a.cij2bgzi6jqj.us-east-1.rds.amazonaws.com",
+    # "database": "weather",
+    # "user": "epam",
+    # "password": "SSpassword",
+    # "port": "5432"
     # "host": "192.168.208.138",
     # "database": "weather",
     # "user": "postgres",
