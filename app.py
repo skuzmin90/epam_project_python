1d7e0c989ddb1a4b03a709b805db71371ba03d2e
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-20 16:51:02 +0300

    Push image to ECR

--- app.py
+++ app.py
@@ -12,13 +12,22 @@ days = [('{:04}/{:02}/{:02}/'.format(datetime.now().year, datetime.now().month, 
 column_names = ["id", "weather_state_name", "wind_direction_compass", "created",
                     "applicable_date", "min_temp", "max_temp", "the_temp"]
 
+# db_params = {
+#     "host": os.getenv('DB_HOST'),
+#     "database": os.getenv('DB_NAME'),
+#     "user": os.getenv('DB_USER'),
+#     "password": os.getenv('DB_PASSWORD'),
+#     "port": "5432"
+# }
+
 db_params = {
-    "host": os.getenv('DB_HOST'),
-    "database": os.getenv('DB_NAME'),
-    "user": os.getenv('DB_USER'),
-    "password": os.getenv('DB_PASSWORD'),
+    "host": "192.168.208.138",
+    "database": "weather",
+    "user": "postgres",
+    "password": "SSpassword",
     "port": "5432"
 }
+
 
 def get_weather_result(city_id, date):
     url = "https://www.metaweather.com/api/location/{}/{}".format(city_id, date)
