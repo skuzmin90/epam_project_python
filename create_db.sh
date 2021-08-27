fcf89ee7e7efdfd443f8413dc9f0732ebd6f5978
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-27 10:34:10 +0300

    Add autoscaling

--- create_db.sh
+++ create_db.sh
@@ -2,6 +2,5 @@
 
 export PGPASSWORD="$DB_PASSWORD"
 
-psql -h "$DB_HOST" -U postgres -tc "SELECT 1 FROM pg_roles WHERE rolname='$DB_USER';" | grep -q 1 || psql -h "$DB_HOST" -U postgres -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
-psql -h "$DB_HOST" -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME';" | grep -q 1 || psql -h "$DB_HOST" -U postgres -c "CREATE DATABASE $DB_NAME;"
-psql -h "$DB_HOST" -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USER;"
+psql -h $DB_HOST -U $DB_USER  postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME';" | grep -q 1 || psql -h $DB_HOST -U $DB_USER  postgres -c "CREATE DATABASE $DB_NAME;"
+psql -h $DB_HOST -U $DB_USER postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USER;"
